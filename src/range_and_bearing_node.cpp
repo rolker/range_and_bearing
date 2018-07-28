09a3131f45d80f5ebafdab6e3ae5a35aed9e3d0e
Roland Arsenault <roland@ccom.unh.edu>
2018-07-28 09:28:19 -0800

    initial commit

---
+++ src/range_and_bearing_node.cpp
@@ -0,0 +1,68 @@
+#include "ros/ros.h"
+#include "sensor_msgs/NavSatFix.h"
+#include "std_msgs/Float32.h"
+#include "project11/mutex_protected_bag_writer.h"
+#include <regex>
+#include "boost/date_time/posix_time/posix_time.hpp"
+#include "project11/gz4d_geo.h"
+
+ros::Publisher rangePub;
+ros::Publisher bearingPub;
+
+double lat1, lon1, lat2, lon2;
+ros::Time ts1, ts2;
+
+MutexProtectedBagWriter log_bag;
+
+void navSatFix1Callback(const sensor_msgs::NavSatFix::ConstPtr& message)
+{
+    ts1 = message->header.stamp;
+    lat1 = message->latitude;
+    lon1 = message->longitude;
+}
+
+void navSatFix2Callback(const sensor_msgs::NavSatFix::ConstPtr& message)
+{
+    ts2 = message->header.stamp;
+    lat2 = message->latitude;
+    lon2 = message->longitude;
+}
+
+void sendRangeAndBearing(const ros::TimerEvent event)
+{
+    if(event.current_real-ts1 < ros::Duration(2.0) && event.current_real-ts2 < ros::Duration(2.0))
+    {
+        ros::Time now = ros::Time::now();
+        gz4d::geo::Point<double,gz4d::geo::WGS84::LatLon> gr(lat1,lon1,0.0);
+        gz4d::geo::LocalENU<> geoReference = gz4d::geo::LocalENU<>(gr);
+        gz4d::Point<double> position = geoReference.toLocal(gz4d::geo::Point<double,gz4d::geo::WGS84::ECEF>(gz4d::geo::Point<double,gz4d::geo::WGS84::LatLon>(lat2,lon2,0.0)));
+        double range = gz4d::norm(position);
+        std_msgs::Float32 rangeMsg;
+        rangeMsg.data = range;
+        rangePub.publish(rangeMsg);
+        log_bag.write("/range",now,rangeMsg);
+    }
+}
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "twist_to_twiststamped");
+    ros::NodeHandle n;
+    
+    boost::posix_time::ptime now = ros::WallTime::now().toBoost();
+    std::string iso_now = std::regex_replace(boost::posix_time::to_iso_extended_string(now),std::regex(":"),"-");
+    std::string log_filename = "nodes/range_and_bearing-"+iso_now+".bag";
+    log_bag.open(log_filename, rosbag::bagmode::Write);
+
+    ros::Subscriber input1 = n.subscribe("/input1",10,&navSatFix1Callback);
+    ros::Subscriber input2 = n.subscribe("/input2",10,&navSatFix2Callback);
+    
+    rangePub = n.advertise<std_msgs::Float32>("/range",1);
+    bearingPub = n.advertise<std_msgs::Float32>("/bearing",1);
+
+    ros::Timer timer = n.createTimer(ros::Duration(1.0),sendRangeAndBearing);
+    ros::spin();
+    
+    return 0;
+    
+}
